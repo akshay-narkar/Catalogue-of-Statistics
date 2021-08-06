eb8c073e7260dec857a26044912cd53fad63460f
akshay-narkar <akshaynarkar1295@gmail.com>
2021-08-06 18:11:10 +0530

    Create temporary actions

---
+++ src/Actions/index.js
@@ -0,0 +1,24 @@
+const CREATE_BOOK = 'CREATE_BOOK';
+const REMOVE_BOOK = 'REMOVE_BOOK';
+const CHANGE_FILTER = 'CHANGE_FILTER';
+
+function createBook(book) {
+  return ({
+    type: CREATE_BOOK,
+    book,
+  });
+}
+
+const removeBook = (index) => ({
+  type: REMOVE_BOOK,
+  index,
+});
+
+const changeFilter = (category) => ({
+  type: CHANGE_FILTER,
+  category,
+});
+
+export {
+  CREATE_BOOK, REMOVE_BOOK, createBook, removeBook, changeFilter, CHANGE_FILTER,
+};
