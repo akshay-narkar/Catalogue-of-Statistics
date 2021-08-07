f38eb0bfd80120e08a7e130f4e2e62fcecbe6ee9
akshay-narkar <akshaynarkar1295@gmail.com>
2021-08-07 22:06:24 +0530

    Create initstate and pass it to reducers as default

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
+import thunk from 'redux-thunk';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './Routes/routes';
 import rootReducer from './Reducers/index';
+import apicall from './API/soccer';
 
 const initstate = {
-  books: [{
-    Id: Math.floor((Math.random() * 1000000)),
-    Title: 'Harry Potter & The Prisoners of Askaban',
-    Category: 'Kids',
-  },
-  { Id: Math.floor((Math.random() * 1000000)), Title: 'Shoe Dog', Category: 'Biography' },
-  { Id: Math.floor((Math.random() * 1000000)), Title: 'The Great Gatsby', Category: 'Action' }],
+  teams: [],
+  teaminfo: [],
 };
 
 const store = createStore(rootReducer, {
-  bookupdateReducer: initstate,
-});
+  teamupdatereducer: initstate,
+  teamdeetsReducer: initstate,
+}, applyMiddleware(thunk));
+
+store.dispatch(apicall);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -32,7 +32,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
