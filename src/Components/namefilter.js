03e9dc80fb1ec02be4032fbe3aef2d411dfbad5c
akshay-narkar <akshaynarkar1295@gmail.com>
2021-08-08 20:29:00 +0530

    Change filter styling

--- src/Components/namefilter.js
+++ src/Components/namefilter.js
@@ -4,10 +4,10 @@ import PropTypes from 'prop-types';
 function NameFilter(props) {
   const { clickHandler, filtercat } = props;
   return (
-    <div className="ms-3 w-50 ">
-      <label htmlFor="exampleFormControlInput1" className="form-label d-flex align-items-center">
+    <div className="ms-5 my-3">
+      <label htmlFor="exampleFormControlInput1" className="form-label d-flex align-items-baseline">
         <p className="m-0">Team Name:</p>
-        <input type="text" className="form-control text-box ms-2 w-75 " value={filtercat} id="exampleFormControlInput1" placeholder="Title" onChange={clickHandler} />
+        <input type="text" className="form-control text-box ms-2 w-50" value={filtercat} id="exampleFormControlInput1" placeholder="Title" onChange={clickHandler} />
       </label>
     </div>
   );
