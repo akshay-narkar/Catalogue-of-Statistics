4f27994599359785f76bdfbcef59403e0bf4e303
akshay-narkar <akshaynarkar1295@gmail.com>
2021-08-08 17:56:33 +0530

    Add change text filter

--- src/Actions/index.js
+++ src/Actions/index.js
@@ -1,6 +1,7 @@
 const ALL_TEAMS = 'ALL_TEAMS';
 const TEAM_DEETS = 'TEAM_DEETS';
 const CHANGE_FILTER = 'CHANGE_FILTER';
+const NAME_CHANGE_FILTER = 'NAME_CHANGE_FILTER';
 
 function allTeams(teams) {
   return ({
@@ -19,6 +20,12 @@ const changeFilter = (category) => ({
   category,
 });
 
+const namechangefilter = (text) => ({
+  type: NAME_CHANGE_FILTER,
+  text,
+});
+
 export {
-  allTeams, teamdeets, ALL_TEAMS, TEAM_DEETS, changeFilter, CHANGE_FILTER,
+  allTeams, teamdeets, ALL_TEAMS, TEAM_DEETS, changeFilter,
+  NAME_CHANGE_FILTER, namechangefilter, CHANGE_FILTER,
 };
