76b4512ff5749381cdb307961c63060decc2362c
akshay-narkar <akshaynarkar1295@gmail.com>
2021-08-09 21:18:38 +0530

    Update styling in maindiv

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -1,15 +1,13 @@
 import { Route, Switch } from 'react-router-dom';
 import React from 'react';
-import Team from '../Components/teampage';
-// import Nav from './Navbar';
+import Team from '../Containers/teampage';
 import App from '../Components/App';
 import Error from '../Components/Error';
 
 export default function Routes() {
   return (
     <>
-      {/* <Nav /> */}
-      <div className="maindiv">
+      <div className="maindiv mt-2 mb-5 px-3">
         <Switch>
           <Route path="/" component={App} exact />
           <Route path="/team" component={Team} />
