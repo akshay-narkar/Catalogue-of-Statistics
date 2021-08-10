4c0e22ec4fcbd7221724fe830e1194f57b566767
akshay-narkar <akshaynarkar1295@gmail.com>
2021-08-10 23:02:02 +0530

    Write ui tests for teampage

--- src/tests/Component Testing/teampage.test.js
+++ src/tests/Component Testing/teampage.test.js
@@ -1,8 +1,19 @@
+/*eslint-disable*/
+
 import renderer from 'react-test-renderer';
-import Error from '../../Components/Error';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import React from 'react';
+import Teampage from '../../Containers/teampage';
+import store from './store';
+
+const props = {
+  calldispatch: () => {},
+  teaminfo: [{ id: 1 }],
+};
 
 it('UI is unaltered wrt first snapshot', () => {
-  const elem = renderer.create(<Error />).toJSON();
+  const elem = renderer.create(<BrowserRouter><Provider store={store}><Teampage props={props} /></Provider></BrowserRouter>).toJSON();
 
   expect(elem).toMatchSnapshot();
 });
