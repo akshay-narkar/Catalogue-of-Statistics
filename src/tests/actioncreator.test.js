675f64d2a61a4aed311abf8ccdbfefbbde28416e
akshay-narkar <akshaynarkar1295@gmail.com>
2021-08-10 13:04:10 +0530

    Add action creator tests

---
+++ src/tests/actioncreator.test.js
@@ -0,0 +1,69 @@
+import configureStore from 'redux-mock-store';
+import {
+  allTeams, teamdeets, changeFilter, namechangefilter,
+} from '../Actions/index';
+
+const mockStore = configureStore();
+const store = mockStore();
+
+describe('Test All actions', () => {
+  beforeEach(() => { // Runs before each test in the suite
+    store.clearActions();
+  });
+
+  describe('Test all teams', () => {
+    test('Dispatches the correct action and payload', () => {
+      const expectedActions = [
+        {
+          teams: 1,
+          type: 'ALL_TEAMS',
+        },
+      ];
+
+      store.dispatch(allTeams(1));
+      expect(store.getActions()).toEqual(expectedActions);
+    });
+  });
+
+  describe('Test Team deets', () => {
+    test('Dispatches the correct action and payload', () => {
+      const expectedActions = [
+        {
+          team: 4,
+          type: 'TEAM_DEETS',
+        },
+      ];
+
+      store.dispatch(teamdeets(4));
+      expect(store.getActions()).toEqual(expectedActions);
+    });
+  });
+
+  describe('Test Change Filter', () => {
+    test('Dispatches the correct action and payload', () => {
+      const expectedActions = [
+        {
+          category: 'England',
+          type: 'CHANGE_FILTER',
+        },
+      ];
+
+      store.dispatch(changeFilter('England'));
+      expect(store.getActions()).toEqual(expectedActions);
+    });
+  });
+
+  describe('Test Name Change Filter', () => {
+    test('Dispatches the correct action and payload', () => {
+      const expectedActions = [
+        {
+          text: 'ABC',
+          type: 'NAME_CHANGE_FILTER',
+        },
+      ];
+
+      store.dispatch(namechangefilter('ABC'));
+      expect(store.getActions()).toEqual(expectedActions);
+    });
+  });
+});
